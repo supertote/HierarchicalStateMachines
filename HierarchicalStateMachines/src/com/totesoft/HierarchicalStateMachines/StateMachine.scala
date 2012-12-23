6f5fde66844ba54285e2f80ae5931d5d9be428a3
supertote <totesoft@gmail.com>
2012-12-23 04:35:33 +0100

    Removed StateMachine concrete class

--- HierarchicalStateMachines/src/com/totesoft/HierarchicalStateMachines/StateMachine.scala
+++
@@ -1,6 +0,0 @@
-//package com.totesoft.HierarchicalStateMachines
-//
-///**
-//  * A concrete implementation of a RootStateMachine
-//  */
-//class StateMachine(override val name: String) extends RootStateMachine
